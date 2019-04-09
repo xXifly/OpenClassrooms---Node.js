558e979c38c5750f8173e626c17d8cb1f6025268
JORON Alexandre <alexandre.joron@berger-levrault.com>
2019-04-09 12:17:55 +0200

    Ajout de commentaires, echappement des caractères HTML dans la todolist partagée.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # OpenClassrooms---Node.js
-Projet réalisé dans le cardre du cours OpenClassrooms "Des applications ultra-rapides avec Node.js".
+
+Projet réalisé dans le cardre du cours OpenClassrooms [Des applications ultra-rapides avec Node.js](https://openclassrooms.com/fr/courses/1056721-des-applications-ultra-rapides-avec-node-js).
+
+## Installation
+
+Une fois le projet téléchargé, ouvrez votre invité de commande préféré à la racine de celui-ci et lancez la commande `npm install` afin d'installer les modules node.js de celui-ci.
+
+Vous pouvez ensuite vous rendre à l'adresse [localhost:8080](http://localhost:8080) afin de lancer l'application web.
+
+## Utilisation
+
+Une fois à l'adresse [localhost:8080](http://localhost:8080), vous devrez vous connecter en entrant votre pseudo dans la bar de navigation avant de pouvoir accéder aux différents exercices réalisés.
--- app.js
+++ app.js
@@ -21,11 +21,11 @@ var sessionMiddleware = session({
 
 var todolist = [
   {
-    "task" : "jambon",
+    "task" : "Faire les courses",
     "done" : false
   },
   {
-    "task" : "beurre",
+    "task" : "Finir le tuto Openclassrooms",
     "done" : true
   }
 ];
@@ -36,24 +36,22 @@ app.use(sessionMiddleware);
 io.use(ios(sessionMiddleware));
 // On applique le parseur de formulaire
 app.use(bodyParser.urlencoded({ extended: false }));
-// On met en place l'uilisate des template hundlebars
+// On met en place l'utilisation des templates hundlebars
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-//Permet la lecture des fichiers css, js et des photos
+// On permet la lecture des fichiers css et js via le repertoire public
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 /** Gestion des routes en-dessous **/
 
 /* Affiche la page d'accueil */
-
 app.get('/home', function (req, res) {
     res.render('home', {pseudo:req.session.pseudo});
 });
 
-/** Partie Todo List **/
-
-/* Affiche la todo list */
+/* Affiche la todolist */
 app.get('/todo', function (req, res) {
     if(req.session.pseudo){
         res.render('todo', {list:req.session.list, pseudo:req.session.pseudo});
@@ -62,7 +60,7 @@ app.get('/todo', function (req, res) {
     }
 });
 
-/* Ajoute une tâche */
+/* Ajoute une tâche à la todolist */
 app.post('/todo/ajouter', function (req, res) {
     if(req.session.pseudo){
         if (req.body && req.body.task != ""){
@@ -74,7 +72,7 @@ app.post('/todo/ajouter', function (req, res) {
     }
 });
 
-/* Supprime une tâche */
+/* Supprime une tâche à la todolist */
 app.get('/todo/supprimer/:id', function (req, res) {
     if(req.session.pseudo){
         if(isNumeric(req.params.id)) {
@@ -86,8 +84,6 @@ app.get('/todo/supprimer/:id', function (req, res) {
     }
 });
 
-/** Partie Super Chat **/
-
 /* Affiche le chat */
 app.get('/chat', function (req, res) {
     if(req.session.pseudo){
@@ -97,7 +93,7 @@ app.get('/chat', function (req, res) {
     }
 });
 
-/* Affiche le chat */
+/* Affiche la todolist partagée */
 app.get('/stodo', function (req, res) {
     if(req.session.pseudo){
         res.render('stodo', {pseudo:req.session.pseudo}); 
@@ -106,27 +102,28 @@ app.get('/stodo', function (req, res) {
     }
 });
 
-
 /* Gère la redirection en cas d'URL incorrect */
 app.use(function (req, res, next) {
     res.redirect('/home');
 });
 
 
-
+/* Mise en place des socket */
 io.sockets.on('connection', function (socket) {
 
+    // On transmet le pseudo aux pages affichées
     socket.emit('pseudo',socket.handshake.session.pseudo)
 
+    // Récupération du pseudo lors de la connexion
     socket.on('login', function(pseudo) {
         if(pseudo != ''){
             socket.handshake.session.pseudo =ent.encode(pseudo);
             socket.handshake.session.list = [];
             socket.handshake.session.save();
         }
-
     });
 
+    // Supression du pseudo lors de la déconnexion
     socket.on('logout', function(pseudo) {
         if (socket.handshake.session.pseudo) {
             delete socket.handshake.session.pseudo;
@@ -135,34 +132,43 @@ io.sockets.on('connection', function (socket) {
         }
     });
 
+    // Gestion d'une nouvelle connexion au chat
     socket.on('new-chat-connexion', function(){
         socket.broadcast.emit('new-chat-connexion', socket.handshake.session.pseudo);
     })
 
+    // Gestion d'un nouveau message sur le chat
     socket.on('new-message', function(message) {
         socket.broadcast.emit('new-message', {pseudo: socket.handshake.session.pseudo, message: ent.encode(message)});
     });
 
+    // Gestion d'une nouvelle connexion à la todolist partagée
     socket.on('new-todo-connexion', function(){
         socket.emit('new-todo-connexion', todolist);
     })
 
-    socket.on('new-task', function(task) {
-        todolist.push({"task":task,"done":false});
-        socket.broadcast.emit('new-task', ent.encode(task));
-    });
+    socket.on('new-task', function(task){
+        todolist.push({task: ent.encode(task), done: false});
+        io.emit('list-update', todolist);
+    })
 
+    // Gestion de la reception d'une nouvelle todolist
     socket.on('list-update',function(list){
         todolist = list;
-        socket.broadcast.emit('list-update', todolist);
+        io.emit('list-update', todolist);
     });
 });
-
 
 server.listen(8080);
 
 /* Fonction permettant de savoir si une variable est numérique */
 function isNumeric(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+/* Fonction permettant d'encoder les valeurs de la todolist */
+function listEncode(list) {
+    list[list.length - 1].task = ent.encode(list[list.length - 1].task);
+    return list;
 }
 
